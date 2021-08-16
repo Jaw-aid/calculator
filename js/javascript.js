cd0c70afa70c4258438a99d117cdcc161c0e3a98
Jaw-aid <jjali88@gmail.com>
2021-08-16 17:09:13 -0500

    Inserted 0 and starting on functions to click btns

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,12 @@
 // javascript
+// TODO Give values to each button
+// TODO Update screen with numbers when button is clicked 
+// TODO Update screen when Clear btn is pressed
+// TODO Update screen when Delete btn is pressed 
+// TODO Add in logic for summing up numbers 
+// TODO Implement other logic (sub, mult, div) 
+// TODO Add in functionality to press buttons for numbers 
+
+function numberPressed(value) {
+    document.getElementById("displayNum")
+}
