4b62735fb526532978e6fb063967600bc6ddf79f
Jaw-aid <jjali88@gmail.com>
2021-08-16 23:19:52 -0500

    Added functionality for number, clear, delete btns

--- js/javascript.js
+++ js/javascript.js
@@ -8,5 +8,28 @@
 // TODO Add in functionality to press buttons for numbers 
 
 function numberPressed(value) {
-    document.getElementById("displayNum")
+    let displayValue = document.getElementById("displayNum").textContent
+    let newValue
+    console.log(value)
+    if (displayValue == 0 && value == 0) {
+        return
+    }
+    if (displayValue == 0 && value > 0) {
+        displayValue = ''
+    }
+    newValue = displayValue.concat(value)
+    document.getElementById("displayNum").textContent = newValue
+}
+
+function clearPressed() {
+    document.getElementById("displayNum").textContent = 0
+}
+
+function deletePressed() {
+    let displayValue = document.getElementById("displayNum").textContent
+    if (document.getElementById("displayNum").textContent.length == 1) {
+        document.getElementById("displayNum").textContent = 0
+        return
+    } 
+    document.getElementById("displayNum").textContent = displayValue.slice(0, displayValue.length - 1)
 }
