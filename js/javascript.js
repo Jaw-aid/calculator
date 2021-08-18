a3de0ed6dd650e526c4bc237fba0788894b3e558
Jaw-aid <jjali88@gmail.com>
2021-08-17 23:59:32 -0500

    Added functionality for each operand

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,29 @@
 // javascript
-// TODO Add in logic for summing up numbers 
-// TODO Implement other logic (sub, mult, div) 
+// TODO Add in functionality to have a string of numbers calculating
 // TODO Add in functionality to press buttons for numbers 
 // TODO Make sure there is no overflow of numbers 
 // TODO find an improved way of calling on the onclick btn with a value passed in
 // TODO how to handle large numbers (use .toExponential)
 
+let n1
+let n2
+let operand
+let operandClear
+
 function numberPressed(value) {
     let displayValue = document.getElementById("displayNum").textContent
     let newValue
-    console.log(value)
+    // console.log(value)
+    // console.log(n1)
     if (displayValue == 0 && value == 0) {
         return
     }
-    if (displayValue == 0 && value > 0) {
+    if (displayValue == '0' && value > 0) {
         displayValue = ''
+    }
+    if (operandClear !== undefined) {
+        displayValue = ''
+        operandClear = undefined
     }
     newValue = displayValue.concat(value)
     document.getElementById("displayNum").textContent = newValue
@@ -22,6 +31,8 @@ function numberPressed(value) {
 
 function clearPressed() {
     document.getElementById("displayNum").textContent = 0
+    num1 = undefined
+    num2 = undefined
 }
 
 function deletePressed() {
@@ -35,6 +46,10 @@ function deletePressed() {
 
 function decimalPressed() {
     let displayValue = document.getElementById("displayNum").textContent
+    if (operandClear !== undefined) {
+        displayValue = ''
+        operandClear = undefined
+    }
     if (displayValue.includes('.')) {
         return
     } else {
@@ -43,11 +58,49 @@ function decimalPressed() {
     }
 }
 
-function add(n1, n2) { return n1 + n2}
-function subtrack(n1, n2) { return n1 - n2}
-function multiply(n1, n2) { return n1 * n2}
-function divide(n1, n2) { return n1 / n2}
+function add() {
+    n1 = document.getElementById("displayNum").textContent
+    operand = '+'
+    console.log(operand)
+    operandClear = 'Addition'
+}
+function subtract() {
+    n1 = document.getElementById("displayNum").textContent
+    operand = '-'
+    console.log(operand)
+    operandClear = 'Subtract'
+}
+function multiply() {
+    n1 = document.getElementById("displayNum").textContent
+    operand = '*'
+    console.log(operand)
+    operandClear = 'Multiply'
+}
+function divide() {
+    n1 = document.getElementById("displayNum").textContent
+    operand = '/'
+    console.log(operand)
+    operandClear = 'Divide'
+}
 
 function operate() {
-
+    n2 = document.getElementById("displayNum").textContent
+    n1Num = Number(n1)
+    n2Num = Number(n2)
+    if (operand == '+') {
+        n1 = n1Num + n2Num
+        return  document.getElementById("displayNum").textContent = n1Num + n2Num
+    }
+    if (operand == '-') {
+        n1 = n1Num - n2Num
+        return  document.getElementById("displayNum").textContent = n1Num - n2Num
+    }
+    if (operand == '*') {
+        n1 = n1Num * n2Num
+        return  document.getElementById("displayNum").textContent = (n1Num * n2Num)
+    }
+    if (operand == '/') {
+        n1 = n1Num / n2Num
+        return  document.getElementById("displayNum").textContent = n1Num / n2Num
+    }
 }
